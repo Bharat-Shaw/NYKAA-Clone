c60407729b56268156065a7ded9906d0a716e83a
rishabh gupta <rishabh.katroliya999@gmail.com>
2023-02-11 12:04:55 +0530

    change

--- signup/otp.js
+++ signup/otp.js
@@ -1,12 +1,11 @@
 document.querySelector("form").addEventListener("submit",saveData);
 var sData=JSON.parse(localStorage.getItem("signInData")) || [];
 
-const defaultOtp=1234;
 function saveData(){
     event.preventDefault();
   
 var otp=document.getElementById("otp").value;
-if(defaultOtp==otp){
+if(otp){
     var data=JSON.parse(localStorage.getItem("dataExist"));
     sData.push(data);
     localStorage.setItem("signInData",sData);
